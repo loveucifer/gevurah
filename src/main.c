976cbbc09c9cec2ca5e3a9df0803099474093d21
loveucifer <134506987+loveucifer@users.noreply.github.com>
2026-05-01 02:01:42 +0530

    smol update , i feel sleepy but i have more to read , i need time , give me more time someone

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <stdint.h>
 #include "display.h"
+#include "vector.h"
+
+const int N_POINTS = 9 * 9 * 9;
+Vec3_t cube_points[N_POINTS];
 
 bool is_running = false; // check init window
 
@@ -23,6 +27,21 @@ void setup(void){
         SDL_TEXTUREACCESS_STREAMING,
         window_width,
         window_height);
+
+
+
+    int point_count = 0;
+    // load array of vectors
+    for (float x= -1; x <= 1 ; x+=0.25) {
+        for (float y = -1 ; y <= 1; y+= 0.25) {
+            for (float z = -1 ; z <= 1; z+= 0.25){
+                Vec3_t first_point = {.x = x,.y= y,.z = z};
+                cube_points[point_count++] = first_point;
+
+
+            }
+        }
+    }
 }
 
 void process_input(void){
@@ -73,6 +92,8 @@ int main(void){
     //SDL window
     is_running = init_window();
     setup();
+
+
     while (is_running) {
         process_input();
         update();
---
+++ src/vector.h
@@ -0,0 +1,22 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+// what we are essentially doing here is creating 2 structs for creating vectors
+// vectors are in 2d and 3d so vec2 is obvi 2d and vec3 is obvi 3d
+
+typedef struct{
+    float x;
+    float y;
+} Vec2_t;
+
+typedef struct{
+    float x;
+    float y;
+    float z;
+} Vec3_t;
+
+
+// todo // add functions to manipualte these vectors
+//
+
+#endif
