ee7748be3615eacfdd30042c8033ee58c12f5795
loveucifer <134506987+loveucifer@users.noreply.github.com>
2026-04-30 14:01:46 +0530

    updating new render and buffers

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 // its objectively better to start these pointers as NULL because they only have value when they are passed in
 
 SDL_Window* window =NULL;
 SDL_Renderer* renderer=NULL;
+
 bool is_running = false;
 
+uint32_t* color_buffer = NULL; // color buffer is a pointer to the first position of array of 32 bit integers of color values
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool init_window(void){
     if (SDL_Init(SDL_INIT_EVERYTHING) !=0 ){
@@ -37,8 +44,8 @@ bool init_window(void){
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
 
@@ -54,7 +61,7 @@ bool init_window(void){
     // window : which is the window that we passed in earlier as a pointer
     // next is the default display device  we use -1 and it just gets the default thing
     // next is just flags , for now we dont want to so just 0
-    //
+
 
 
     renderer = SDL_CreateRenderer(window, -1, 0);
@@ -67,11 +74,22 @@ bool init_window(void){
 
 
 
-
-
+//SETUP
+// ok so here we are doing the color buffers , what we need to understand is that we are allocating color buffers
+// for each pixel that is set according to the window width and hieght that we initalized at the top
+// and then set a texture , the texture accepts 5 parameters , first one is where we want it which is the renderer
+// then the pixel format , acces and then the height and width of the texture
 
 void setup(void){
-        //TODO
+
+    color_buffer = (uint32_t*)malloc(sizeof(uint32_t)* window_width * window_height);
+
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height);
 }
 
 
@@ -82,8 +100,6 @@ void setup(void){
 void process_input(void){
     SDL_Event event;
     SDL_PollEvent(&event);
-
-
     // here we are passing in the address of the event declared above
     // we use a swtich statement to check the conditions that make is_running false so we can stop the game loop
     // here the first case is SDL_QUIT which is checking if we need to quit and then updates it down below the is_running
@@ -91,9 +107,6 @@ void process_input(void){
     // yeah the closing red button window , THAT.
     // and if you can read the second case its just pressed down key and if the presseed down key is infact escape
     // you should also stop the loop whihc is is_running false
-
-
-
     switch (event.type) {
         case SDL_QUIT:
             is_running = false;
@@ -118,11 +131,40 @@ void update(void){
         //TODO
 }
 
+// rendering color buffer first we use the sdl update texture function we lets us update the texture
+// here it accpets arguments first one we use is what texutre what we want to use which is ofc the
+// color_buffer_texture we setup next is if we want any rectangular subdivision which is null for us
+// and then what color buffer we want to copy to the texture whihc is basically our color_buffer
+// and last is pitch wehcih ,means how many bytes we have in one row waahere we use the ( window width * sizeofint32)
+
+void render_color_buffer(void){
+
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)window_width * sizeof(uint32_t)
+        );
+
+    SDL_RenderCopy(
+        renderer,
+        color_buffer_texture,
+        NULL,
+        NULL);
+
+}
 
 
+// Clear color buffer
 
+void clear_color_buffer(uint32_t color){
+    for (int y = 0; y < window_height; y++ ){
+        for (int x =0; x< window_width; x++) {
+            color_buffer[(window_width *y) + x]  = color;
 
-
+        }
+    }
+}
 
 // RENDER
 // SDL_SetRenderDrawColor lets you pass in the colour you want it to show up on the scren
@@ -135,9 +177,25 @@ void update(void){
 void render(void){
     SDL_SetRenderDrawColor(renderer,255,0,0,255);
     SDL_RenderClear(renderer);
+    render_color_buffer();
+    clear_color_buffer(0XFFFFFF00);
     SDL_RenderPresent(renderer);
 }
 
+// DESTTRUCTION
+// this is a function that destroys the things we allocated memeory to , malloc is the way
+// we manually allocate memory and we free them with basically calling " free ", but here last
+// time i didnt cleanup the renderer and the window so i needed to do that with sdl as well
+// if you look at it its exactly reverse of whatever we created in initialize window first we created the window \
+// then we created a new renderer and then we did the color buffer so exact backtrack here folks
+// so like as above so below
+
+void destruct_window(void){
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 
 
@@ -161,5 +219,10 @@ int main(void){
         render();
 
     }
+
+
+    destruct_window();
+
+
     return 0;
 }
