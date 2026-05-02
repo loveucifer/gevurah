bb9de605d5e52340dea8fde853d9a2d0874eae40
loveucifer <134506987+loveucifer@users.noreply.github.com>
2026-05-03 00:32:53 +0530

    thank god

--- src/mesh.h
+++ src/mesh.h
@@ -4,22 +4,30 @@
 #include "vector.h"
 
 
-#define N_MESH_VERTICES 8
-extern Vec3_t mesh_vertices[N_MESH_VERTICES];
+#define N_CUBE_VERTICES 8
+extern Vec3_t cube_vertices[N_CUBE_VERTICES];
 
 
-#define N_MESH_FACES (6*2) // 6 cube faces 2 traingles per face
-extern face_t mesh_faces[N_MESH_FACES];
+#define N_CUBE_FACES (6*2) // 6 cube faces 2 traingles per face
+extern face_t cube_faces[N_CUBE_FACES];
+
+//////////////////////////////////////////////////////////////////////
+/// defines a struct for dynamic size meshes with array of vertices and faces
+/// ????????????????????????????????????????????????????????
+
+typedef struct {
+    Vec3_t* vertices;  // dynamic array of vertices and faces
+    face_t* faces;
+    Vec3_t rotation;  /// rotation with x y z value
+} mesh_t;
+
+extern mesh_t mesh;  // global mesh variable
+
+
+void load_cube (void);
+
 
 #endif
-
-
-
-
-
-
-
-
 
 
 //
