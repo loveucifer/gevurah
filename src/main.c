33fd8e29974db387bca8a24d1b5e42ece3dd5329
loveucifer <134506987+loveucifer@users.noreply.github.com>
2026-05-04 23:43:03 +0530

    bubble sort

--- src/main.c
+++ src/main.c
@@ -245,6 +245,17 @@ void update(void){
         array_push(triangles_to_render, projected_triangle);
     }
     // sort triangles to render by avg depth in ascending order
+    int num_triangles = array_length(triangles_to_render);
+    for(int i = 0; i < num_triangles ; i++){
+        for (int j = 0; j < num_triangles; j++){
+            if (triangles_to_render[i].avg_depth < triangles_to_render[j].avg_depth) {
+                triangle_t temp = triangles_to_render[i];
+                triangles_to_render[i] = triangles_to_render[j];
+                triangles_to_render[j] = temp;
+
+            }
+        }
+    }
 }
 
 void render(void){
