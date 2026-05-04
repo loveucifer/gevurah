a2b7f514603a5f8375a66d55688e3ff668ef48c9
loveucifer <134506987+loveucifer@users.noreply.github.com>
2026-05-04 13:01:46 +0530

    nuklear

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 build:
 	gcc -Wall -std=c99 -I/opt/homebrew/opt/sdl2/include ./src/*c -L/opt/homebrew/opt/sdl2/lib -lSDL2 -lm -o gevurah
 
+
 run:
 	./gevurah
 
--- src/triangle.c
+++ src/triangle.c
@@ -12,7 +12,6 @@ void swap(int* a , int* b ){
 
 void fill_flat_bottom(int x0 , int y0 , int x1 , int y1 , int x2 , int y2 , uint32_t color  ){
 
-
     float inv_slope_1 = (float) (x1 - x0) / (y1-y0);
     float inv_slope_2 =(float) (x2 - x0) / (y2 - y0);
 
@@ -27,9 +26,29 @@ void fill_flat_bottom(int x0 , int y0 , int x1 , int y1 , int x2 , int y2 , uint
         x_end += inv_slope_2;
     }
 };
+
+
 void fill_top_bottom(int x0 , int y0 , int x1 , int y1 , int x2 , int y2 , uint32_t color ){
-// TODO
+
+    float inv_slope_1 = (float) (x2 - x0) / (y2-y0);
+    float inv_slope_2 =(float) (x2 - x1) / (y2 - y1);
+
+
+    // start from bottom vertex this time
+
+    float x_start = x2;
+    float x_end = x2;
+
+    for (int y = y2; y >= y0 ; y--){
+
+        draw_line(x_start, y, x_end, y, color);
+        x_start -= inv_slope_1;
+        x_end -= inv_slope_2;
+    }
 };
+
+
+
 
 void draw_filled_triangle(int x0, int y0, int x1, int y1 , int x2, int y2, uint32_t color) {
     if (y0 > y1) {
@@ -44,6 +63,11 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1 , int x2, int y2, uint3
         swap(&y0 , &y1);
         swap(&x0 , &x1);
     }
+    if (y1 == y2) {
+        fill_flat_bottom(x0 , y0 , x1 , y1, x2 , y2 , color);
+    } else if (y0 == y1) {
+        fill_top_bottom( x0,  y0, x1,  y1,  x2,  y2, color);
+    } else {
 
     // need to calc mid point with triangle similarity
 
@@ -53,6 +77,9 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1 , int x2, int y2, uint3
     // draw flat bottom triangle and draw flat top triangle
 
     fill_flat_bottom(x0 ,y0 ,x1 ,y1 ,Mx ,My, color);
+
     fill_top_bottom(x1 ,y1 ,Mx ,My , x2 , y2, color);
+
+    }
 
 }
